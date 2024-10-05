1c48fe1749489de8318e7cd40179c6571c36d348
iman <dev.imansabet@gmail.com>
2024-10-05 21:52:06 +0330

    implementing slug-based retrieval

--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -31,6 +31,12 @@ namespace Movies.Application.Repositories
             return Task.FromResult(movie);
         }
 
+        public Task<Movie?> GetBySlugAsync(string slug)
+        {
+            var movie = _movies.SingleOrDefault(x => x.Slug == slug);
+            return Task.FromResult(movie);
+        }
+
         public Task<bool> UpdateAsync(Movie movie)
         {
             var movieIndex = _movies.FindIndex(x => x.Id == movie.Id);
