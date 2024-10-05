a0a0dd738d8089576c4393a16b6a9499ca5f9803
iman <dev.imansabet@gmail.com>
2024-10-05 22:40:51 +0330

    Adding Db infrastructure code

--- Movies.Application/ApplicationServiceCollectionExtensions.cs
+++ Movies.Application/ApplicationServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Database;
 using Movies.Application.Repositories;
 
 namespace Movies.Application;
@@ -10,5 +11,15 @@ public static class ApplicationServiceCollectionExtensions
         services.AddSingleton<IMovieRepository, MovieRepository>();
         return services;
     }
+    public static IServiceCollection AddDatabase(this IServiceCollection services,string connectionString)
+    {
+        services.AddSingleton<IDbConnectionFactory>(_ => 
+            new NpgsqlConnectionFactory(connectionString));
+
+        services.AddSingleton<DbInitializer>();
+
+        return services;
+
+    }
+
 }
- 
